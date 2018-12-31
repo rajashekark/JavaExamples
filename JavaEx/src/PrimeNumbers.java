9ff77208c4a6dc377628e522a045b46e7705a631
Rajashekar <rajashekarreddy339@gmail.com>
2018-12-31 11:20:25 +0530

    first commit for prime numbers

---
+++ JavaEx/src/PrimeNumbers.java
@@ -0,0 +1,18 @@
+
+public class PrimeNumbers {
+
+	public static void main(String args[])
+	{
+		int num = 4;
+		if(num!=2 && num%2==0)
+	    {
+			System.out.println("Not a PrimeNumber");
+		}
+		else 
+		{
+			
+		}
+		
+	}
+
+}
