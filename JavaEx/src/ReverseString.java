557cf61c7229591910429332a7fa4bc6fcaf2086
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-02 15:44:31 +0530

    reverseString

--- JavaEx/src/ReverseString.java
+++ JavaEx/src/ReverseString.java
@@ -12,6 +12,23 @@ public class ReverseString {
 			reverse = reverse + stmt.charAt(i);
 		}
 		System.out.println("reversed String:" + reverse);
+		
+		//Split the string into words
+		
+		System.out.println("enter the string:");
+		String[] stmt2 = s.nextLine().split(" ");
+		System.out.println(stmt2.length);
+		for(i=stmt2.length-1;i>=0;i--) {
+			System.out.println(stmt2[i]);//in reverse direction
+		}
+		for(i=0;i<=stmt2.length-1;i++) {
+			System.out.println(stmt2[i]);//in forward direction
+		}
+		
+		//reverse the words in their respective positions
+		System.out.println("Enter the statment:");
+		String stmnt3 = s.nextLine();
+		
 	}
 
 }
