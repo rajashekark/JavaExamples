8a8d1024334df1b54a2ba15c0b8c008e2e0c569e
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-08 11:22:23 +0530

    reverse words in string

---
+++ JavaEx/src/strings/ReverseWordsInString.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ReverseWordsInString {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String string = s.nextLine();
+		System.out.println("Entered String is:"+string.length());
+		//for(int i=string.length(); i>=0; i--) {
+			
+		//}
+	}
+
+}
