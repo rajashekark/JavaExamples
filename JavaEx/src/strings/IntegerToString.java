4e8415221a83aa0b3cc4fa74b2915dd1f7f17338
Rajashekar <rajashekarreddy339@gmail.com>
2019-01-08 11:21:25 +0530

    integer to string

---
+++ JavaEx/src/strings/IntegerToString.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class IntegerToString {
+	public static void main(String args[]) {
+		int num = 1234;
+		System.out.println(Integer.toString(num));
+		System.out.println(Integer.valueOf(num));
+	}
+
+}
